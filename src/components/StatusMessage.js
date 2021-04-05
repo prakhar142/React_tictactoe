a64d1b1fe502bcdd5f5ade50925a08d138d4742b
prakhar142 <mintsnpjs@gmail.com>
2021-04-05 16:35:07 +0530

    conditional rendering - dsiplayed tie msg

---
+++ src/components/StatusMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StatusMessage = ({ winner, current }) => {
+  const noMovesLeft = current.board.every(el => el !== null);
+  return (
+    <h2>
+      {winner && `Winner is ${winner}`}{' '}
+      {!winner && !noMovesLeft`Next Player is ${current.isXNext ? 'X' : 'O'}`}
+      {!winner && noMovesLeft && 'X and O ties'}
+    </h2>
+  );
+};
+
+export default StatusMessage;
